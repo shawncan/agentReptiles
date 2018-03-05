2fdc350efac798350da93f6e60c51a2b3424aa4f
王佳灿 <wangjiacan0216@gmail.com>
2018-03-05 22:43:10 +0800

    无

---
+++ tool.py
@@ -0,0 +1,129 @@
+#!/usr/local/Cellar/python3
+# -*- coding: utf-8 -*-
+
+import pymysql
+import configparser
+import requests
+import logging
+
+
+class Log(object):
+    #===========================================================================
+    # '''
+    # 日志操作
+    # logger: 建立日志
+    # '''
+    #===========================================================================
+    def logger(self):
+        logging.basicConfig(level=logging.WARNING,
+                            format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                            datefmt='%Y-%m-%d %H:%M:%S',
+                            filename='/Users/wangjiacan/Desktop/sourceCode/log/agentReptiles.txt',
+                            filemode='a')
+
+        logger = logging.getLogger()
+
+        return logger
+
+class MYSQL(object):
+    #===========================================================================
+    # '''
+    # 数据库操作
+    # getCursor: 数据库连接, queryOperation: 查询数据库, executeOperation: 写入数据库
+    # '''
+    #===========================================================================
+    def __init__(self):
+        conf = configparser.ConfigParser()
+        conf.read("/Users/wangjiacan/Desktop/sourceCode/configurationFile/localConfiguration.ini")
+
+        self.host = conf.get("localServer", "host")
+        self.port = int(conf.get("localServer", "port"))
+        self.user = conf.get("localServer", "user")
+        self.password = conf.get("localServer", "password")
+        self.dbName = conf.get("localServer", "dbname")
+
+    def getCursor(self):
+        # 建立数据库链接
+        self.db = pymysql.connect(host=self.host, port=self.port, user=self.user, password=self.password, db=self.dbName, charset='utf8')
+
+        # 创建游标对象
+        cursor = self.db.cursor()
+
+        # 返回游标对象
+        return cursor
+
+    def queryOperation(self,sql):
+        # 建立连接获取游标对象
+        cur = self.getCursor()
+
+        # 执行SQL语句
+        cur.execute(sql)
+
+        # 获取查询数据
+        # fetch*
+        # all 所有数据, one 取结果的一行, many(size),去size行
+        dataList = cur.fetchall()
+
+        # 关闭游标
+        cur.close()
+
+        # 关闭数据库连接
+        self.db.close()
+
+        # 返回查询数据
+        return dataList
+
+    def executeOperation(self,sql):
+        # 操作状态
+        operatingStatus = 0
+
+        # 建立连接获取游标对象
+        cur = self.getCursor()
+
+        try:
+            # 执行SQL语句
+            cur.execute(sql)
+
+            # 提交修改
+            self.db.commit()
+        except Exception as error:
+            print(error)
+
+            # 错误回滚
+            self.db.rollback()
+
+            # 更改操作状态
+            operatingStatus = 1
+
+        # 关闭游标
+        cur.close()
+
+        # 关闭数据库连接
+        self.db.close()
+
+        return operatingStatus
+
+
+class WebDownload(Log):
+    #===========================================================================
+    # '''
+    # 网页下载操作
+    # getHTMLText: 下载网页源码
+    # '''
+    #===========================================================================
+    def getHTMLText(self, url, code='utf-8'):
+        pass
+        headers = {
+            'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36',
+        }
+
+        try:
+            r = requests.get(url, headers=headers, timeout=20)
+            r.raise_for_status()
+            r.encoding = code
+            print(r.status_code)
+            return r.text
+        except Exception:
+            logger = self.logger()
+            logger.exception("Download HTML Text failed")
+
---
+++ xicidaili.py
@@ -0,0 +1,141 @@
+#!/usr/local/Cellar/python3
+# -*- coding: utf-8 -*-
+
+import re
+from bs4 import BeautifulSoup
+import time
+import tool
+
+class xicidaili(object):
+
+    def __init__(self):
+        self.aimsUrlList = ['http://www.xicidaili.com/nn/', 'http://www.xicidaili.com/nt/', 'http://www.xicidaili.com/wn/', 'http://www.xicidaili.com/wt/']
+        self.proxyList = []
+        self.download = tool.WebDownload()
+        self.my = tool.MYSQL()
+        self.success = 0
+        self.failure = 0
+        self.repeat = 0
+        self.crawlQuantity = 0
+
+    def writeDatabase(self):
+        # 写入sql语句
+        write_sql = """
+        insert into agent_pool (ip, port, type, verification_time) VALUES ("{ip}", "{port}", "{ipType}", "{verificationTime}")
+        """
+        # 查询sql语句
+        inquire_sql = """
+        select * from agent_pool WHERE ip = "{ip}" and port = "{port}"
+        """
+
+        # 循环插入ip数据
+        for infoData in self.proxyList:
+            # 提取每个字典数据
+            ip = infoData["ip"]
+            port = infoData["端口"]
+            ipType = infoData["类型"]
+            verificationTime = infoData["验证时间"]
+
+            # 查询是否已存在ip信息
+            searchResult = self.my.queryOperation(inquire_sql.format(ip=ip, port=port))
+
+            # 插入ip信息
+            if not searchResult:
+                writeState = self.my.executeOperation(write_sql.format(ip=ip, port=port, ipType=ipType, verificationTime=verificationTime))
+
+                # 记录插入情况
+                if writeState == 0:
+                    self.success += 1
+                else:
+                    self.failure += 1
+            else:
+                self.repeat += 1
+                continue
+
+
+
+    def extractIp(self, url):
+        # 获取开始小时与结束小时
+        endHour = int(time.strftime("%H", time.localtime()))
+        startHour = endHour - 2
+
+        # 获取源码并解析
+        html = self.download.getHTMLText(url)
+        soup = BeautifulSoup(html, 'html.parser')
+
+        # 提取table标签下内容
+        table = soup.find('table')
+        # 提取tr标签下内容，信息所在标签
+        tr = table.find_all('tr')
+
+        for info in tr[1:]:
+            proxyData = {'ip': '', '端口': '', '类型': '', '验证时间': '', }
+
+            # 获取td标签下内容
+            trContent = re.findall(r'<td>.*</td>', str(info))
+
+            # 获取验证时间
+            verificationTime = trContent[4][4:-5]
+            # 获取验证的小时
+            hour = int(verificationTime.split(" ")[1].split(":")[0])
+            # 当前小时小于等于开始小时的跳过
+            if hour <= startHour :
+                continue
+            # 当前小时大于等于结束小时的跳过
+            if hour >= endHour :
+                continue
+
+
+            # 获取bar所属div标签下的内容
+            filterContent = re.findall(r'<div class="bar" .*>', str(info))
+            # 获取连接速度
+            speed = filterContent[0].split('"')[3][:-1]
+            # 获取连接时间
+            connect_time = filterContent[1].split('"')[3][:-1]
+            # 判断连接速度大于3秒跳过
+            if float(speed) > 3:
+                continue
+            # 判断连接时间大于1秒跳过
+            if float(connect_time) > 1:
+                continue
+
+
+            # ip地址
+            ip = trContent[0][4:-5]
+            # 端口
+            port = trContent[1][4:-5]
+            # 类型
+            ipType = trContent[2][4:-5]
+
+            proxyData['ip'] = ip
+            proxyData['端口'] = port
+            proxyData['类型'] = ipType
+            proxyData['验证时间'] = verificationTime
+
+            # 添加代理字典到代理列表中
+            self.proxyList.append(proxyData)
+
+            # 记录爬取成功数
+            self.crawlQuantity += 1
+
+
+    def start(self):
+        # 名称
+        scriptName = "http://www.xicidaili.com/"
+        # 操作时间
+        operatingTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
+        for url in self.aimsUrlList:
+            self.extractIp(url)
+        self.writeDatabase()
+
+        print("操作网站：{scriptName}".format(scriptName=scriptName))
+        print("操作时间：{operatingTime}".format(operatingTime=operatingTime))
+        print("操作结果：爬取{crawlQuantity}个代理，入库成功{success}个代理，入库失败{failure}个代理, ，入库重复{repeat}个代理".format(
+            crawlQuantity=self.crawlQuantity, success=self.success, failure=self.failure, repeat=self.repeat))
+
+
+
+if __name__ == '__main__':
+    extract = xicidaili()
+    extract.start()
