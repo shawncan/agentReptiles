052ffb2d8e92d044ac74cb0096c304958efd412e
王佳灿 <wangjiacan0216@gmail.com>
2018-03-12 22:01:44 +0800

    删除了不要的信息

--- tool.py
+++ tool.py
@@ -112,7 +112,6 @@ class WebDownload(Log):
     # '''
     #===========================================================================
     def getHTMLText(self, url, code='utf-8'):
-        pass
         headers = {
             'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36',
         }
