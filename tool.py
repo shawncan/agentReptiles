d4d01f6e66a623812a34104b2e13678b1b01f64a
王佳灿 <wangjiacan0216@gmail.com>
2018-03-16 14:40:04 +0800

    增加：

--- tool.py
+++ tool.py
@@ -5,6 +5,8 @@ import pymysql
 import configparser
 import requests
 import logging
+import smtplib
+from email.mime.text import MIMEText
 
 
 class Log(object):
@@ -111,6 +113,70 @@ class WebDownload(Log):
     # getHTMLText: 下载网页源码
     # '''
     #===========================================================================
+    def __init__(self):
+        self.proxyData = []
+        self.quantity = 0
+        self.my = MYSQL()
+
+
+    def deleteData(self, ip, port):
+        # 删除sql语句
+        delete_sql = """delete from agent_pool where (ip="{ip}" and port="{port}")"""
+
+        # 删除无效Ip
+        self.my.executeOperation(delete_sql.format(ip=ip, port=port))
+
+    def extractData(self):
+        # 查询sql语句
+        inquire_sql = """SELECT ip, port from agent_pool """
+
+        # 查询获取代理Ip库数据
+        self.proxyData = self.my.queryOperation(inquire_sql)
+
+        # 获取代理Ip总数
+        self.quantity = len(self.proxyData)
+
+
+    def proxyGetHTMLText(self, url, code='utf-8'):
+        self.extractData()
+        html = ""
+
+        headers = {
+            'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36',
+        }
+
+        for info in self.proxyData:
+            ip = info[0]
+            port = info[1]
+            poxyIp = ("{ip}:{port}").format(ip=ip, port=port)
+
+            proxies = {
+                "http": "http://" + poxyIp,
+                "https": "http://" + poxyIp
+            }
+
+            try:
+                r = requests.get(url, headers=headers, proxies=proxies, timeout=20)
+                status = r.status_code
+
+                if status == 460:
+                    # 删除失败代理Ip信息
+                    self.deleteData(ip, port)
+                    continue
+
+                r.raise_for_status()
+                r.encoding = code
+                html = r.text
+                break
+            except Exception:
+                logger = self.logger()
+                logger.exception("Download HTML Text failed")
+                # 删除失败代理Ip信息
+                self.deleteData(ip, port)
+
+        return html
+
+
     def getHTMLText(self, url, code='utf-8'):
         headers = {
             'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36',
@@ -125,4 +191,45 @@ class WebDownload(Log):
         except Exception:
             logger = self.logger()
             logger.exception("Download HTML Text failed")
+
+
+class Mail(Log):
+    #===========================================================================
+    # '''
+    # 各类邮件发送操作
+    # agentExhaustedEmail: 代理Ip耗尽邮件
+    # '''
+    #===========================================================================
+    def __init__(self):
+        # 获取配置信息
+        conf = configparser.ConfigParser()
+        conf.read("/Users/wangjiacan/Desktop/sourceCode/configurationFile/localConfiguration.ini")
+
+        self.account = conf.get("singleTimer", "account")
+        self.password = conf.get("singleTimer", "password")
+        self.recipient = conf.get("singleTimer", "recipient")
+
+
+    def agentExhaustedEmail(self, scriptName):
+        # 邮件内容
+        content = "<font color=#000000><br>代理Ip已耗尽，请登录服务器手动抓取代理Ip。<br>运行失败脚本：{scriptName}</font>"
+        # 邮件正文
+        msg = MIMEText(content.format(scriptName=scriptName), 'html', 'utf-8')
+        # 邮件标题
+        msg["Subject"] = "代理Ip耗尽邮件"
+        # 发件人
+        msg["From"] = self.account
+        # 收件人
+        msg["To"] = self.recipient
+
+        try:
+            # 发送邮件
+            server = smtplib.SMTP_SSL("smtp.qq.com", 465)
+            server.login(self.account, self.password)
+            server.sendmail(self.account, self.recipient, msg.as_string())
+            server.quit()
+        except Exception:
+            # 记录错误日志
+            logger = self.logger()
+            logger.exception("Because the mail sending error can not be reminded to the developer, to stop the program running")
 
